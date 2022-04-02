36143e24618993b12b84b797bef2c574252de2bf
gajendra sahu <gajendravmy22@gmail>
2022-04-03 03:26:06 +0530

    url short]

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,7 +1,7 @@
 import { codeGen } from '../utils/codeGen.js';
 import validUrl from 'valid-url';
 import { isValidId, isValid } from '../utils/validations.js';
-// const shortid = require("shortid");
+// import shortid from "shortid";
 
 import userModel from '../models/userModel.js';
 import urlModel from '../models/urlModel.js';
@@ -11,122 +11,120 @@ const baseUrl = 'http://localhost:3000';
 
 //Post Api to Create Longer to Shorten URL...
 const createUrl = async function (req, res) {
-  try {
-    const { body, params, decode } = req;
+	try {
+		const { body, params, decode } = req;
 
-    if (!(Object.keys(body).length > 0)) {
-      // Checking Body is not Empty
-      return res.status(400).send('enter your long url.');
-    }
+		if (!(Object.keys(body).length > 0)) {
+			// Checking Body is not Empty
+			return res.status(400).send('enter your long url.');
+		}
 
-    const { userId } = params;
+		const { userId } = params;
 
-    if (decode._id !== userId) {
-      return res.status(403).send({ status: 'unAuthorized', message: 'please logIn again!' });
-    }
+		if (decode._id !== userId) {
+			return res.status(403).send({ status: 'unAuthorized', message: 'please logIn again!' });
+		}
 
-    const user = await userModel.findOne({ _id: userId, userVerified: true });
+		const user = await userModel.findOne({ _id: userId, userVerified: true });
 
-    if (!user) {
-      return res.status(400).send({
-        status: false,
-        message: 'please create an account or verify your email',
-      });
-    }
+		if (!user) {
+			return res.status(400).send({
+				status: false,
+				message: 'please create an account or verify your email',
+			});
+		}
 
-    const { longUrl, title } = req.body;
+		const { longUrl, title } = req.body;
 
-    if (!isValid(title)) {
-      return res.status(400).send({ status: false, message: 'enter title for url !' });
-    }
+		if (!isValid(title)) {
+			return res.status(400).send({ status: false, message: 'enter title for url !' });
+		}
 
-    if (!isValid(longUrl)) {
-      return res.status(400).send({ status: false, message: 'enter your long url !' });
-    }
+		if (!isValid(longUrl)) {
+			return res.status(400).send({ status: false, message: 'enter your long url !' });
+		}
 
-    if (validUrl.isUri(longUrl)) {
-      // const urlToken = shortid.generate();
-      const urlToken = codeGen(5);
-      let checkUrl = await urlModel
-        .findOne({
-          userId: userId,
-          longUrl: longUrl,
-        })
-        .select({ title: 1, shortUrl: 1, _id: 0 });
+		if (validUrl.isUri(longUrl)) {
+			// const urlToken = shortid.generate();
+			const urlToken = codeGen(5);
+			let checkUrl = await urlModel
+				.findOne({
+					userId: userId,
+					longUrl: longUrl,
+				})
+				.select({ title: 1, shortUrl: 1, _id: 0 });
 
-      if (checkUrl) {
-        return res.send({
-          message: ' You already created Short Url for this Long Url :',
-          data: checkUrl,
-        });
-      } else {
-        const shortUrl = baseUrl + '/' + urlToken;
+			if (checkUrl) {
+				return res.send({
+					message: ' You already created Short Url for this Long Url :',
+					data: checkUrl,
+				});
+			} else {
+				const shortUrl = baseUrl + '/' + urlToken;
 
-        const storedData = {
-          title,
-          longUrl,
-          shortUrl,
-          urlCode: urlToken,
-          userId: userId,
-        };
+				const storedData = {
+					title,
+					longUrl,
+					shortUrl,
+					urlCode: urlToken,
+					userId: userId,
+				};
 
-        let savedData = await urlModel.create(storedData);
-        const returnData = {
-          userName: user.userName,
-          title: savedData.title,
-          shortUrl: savedData.shortUrl,
-        };
-        return res.status(200).send({ status: true, data: returnData });
-      }
-    } else {
-      return res.status(400).send({ status: false, message: 'Invalid Long Url' });
-    }
-  } catch (error) {
-    return res.status(500).send({ message: 'Something wrong from our-end ', error: error.message });
-  }
+				let savedData = await urlModel.create(storedData);
+				const returnData = {
+					userName: user.userName,
+					title: savedData.title,
+					shortUrl: savedData.shortUrl,
+				};
+				return res.status(200).send({ status: true, data: returnData });
+			}
+		} else {
+			return res.status(400).send({ status: false, message: 'Invalid Long Url' });
+		}
+	} catch (error) {
+		return res.status(500).send({ message: 'Something wrong from our-end ', error: error.message });
+	}
 };
 
 async function getAllShortUrls(req, res) {
-  try {
-    const { params, decode } = req;
+	try {
+		const { params, decode } = req;
 
-    if (isValidId(params.userId)) {
-      return res.status(400).send({ status: 'invalid', message: 'invalid userID!' });
-    }
+		if (isValidId(params.userId)) {
+			return res.status(400).send({ status: 'invalid', message: 'invalid userID!' });
+		}
 
-    console.log(params.userId);
-    const user = await userModel
-      .findOne({ _id: params.userId })
-      .select({ _id: 0, password: 0, __v: 0, userVerified: 0 });
+		console.log(params.userId);
+		const user = await userModel
+			.findOne({ _id: params.userId })
+			.select({ _id: 0, password: 0, __v: 0, userVerified: 0 });
 
-    // console.log(user)
+		// console.log(user)
 
-    if (!user) {
-      return res.status(400).send({ status: false, message: 'user dose not exist' });
-    }
+		if (!user) {
+			return res.status(400).send({ status: false, message: 'user dose not exist' });
+		}
 
-    if (user.userVerified === false) {
-      return res.status(400).send({ status: false, message: 'user not verified' });
-    }
+		if (user.userVerified === false) {
+			return res.status(400).send({ status: false, message: 'user not verified' });
+		}
 
-    console.log(decode._id, params.userId);
-    if (decode._id !== params.userId) {
-      return res.status(403).send({ status: 'unauthorized', message: 'please login again!' });
-    }
+		if (decode._id !== params.userId) {
+			return res.status(403).send({ status: 'unauthorized', message: 'please login again!' });
+		}
 
-    const urls = await urlModel
-      .find({ userId: params.userId })
-      .select({ title: 1, shortUrl: 1, _id: 0 });
+		const urls = await urlModel
+			.find({ userId: params.userId })
+			.select({ title: 1, shortUrl: 1, _id: 0 });
 
-    console.log(urls);
-    let userData = JSON.parse(JSON.stringify(user));
-    userData['urls'] = urls.length === 0 ? 'you not generated any short url ' : urls;
+		let userData = JSON.parse(JSON.stringify(user));
+		userData['urls'] = urls.length === 0 ? 'you not generated any short url ' : urls;
 
-    res.status(200).send({ status: 'fetched', data: userData });
-  } catch (error) {
-    res.status(500).send({ status: false, message: 'something wrong from our end!' });
-    throw new Error(error.message);
-  }
+		res.status(200).send({ status: 'fetched', data: userData });
+	} catch (error) {
+		res.status(500).send({ status: false, message: 'something wrong from our end!' });
+		throw new Error(error.message);
+	}
 }
 
 export { createUrl, getAllShortUrls };
